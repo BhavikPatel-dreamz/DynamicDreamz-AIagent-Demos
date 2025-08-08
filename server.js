c16cfd927c5de4ac7404399c768b2b2022f6344a
Bhavik Patel <bhavikpate2010@gmail.com>
2025-08-08 10:48:14 +0530

    Fit push

--- server.js
+++ server.js
@@ -4,12 +4,12 @@
     const fs = require('fs');
 
     const dev = process.env.NODE_ENV !== 'production';
-    const app = next();
+    const app = next({ dev});
     const handle = app.getRequestHandler();
 
     const httpsOptions = {
-      key: fs.readFileSync('./certificates/localhost-key.pem'), // Path to your SSL key
-      cert: fs.readFileSync('./certificates/localhost.pem'), // Path to your SSL certificate
+      key: fs.readFileSync('./certificates/privkey.pem'), // Path to your SSL key
+      cert: fs.readFileSync('./certificates/fullchain.pem'), // Path to your SSL certificate
     };
 
     app.prepare().then(() => {
@@ -18,6 +18,6 @@
         handle(req, res, parsedUrl);
       }).listen(4001, (err) => {
         if (err) throw err;
-        console.log('> Ready on https://localhost:3000');
+        console.log('> Ready on https://localhost:4001');
       });
     });
